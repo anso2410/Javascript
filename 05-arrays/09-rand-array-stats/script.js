9d15c99b0e35b512c9750b66c7368618a48c2b98
as <simana.annesophie@gmail.com>
2020-06-10 10:42:40 +0200

    5-9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -10,7 +10,52 @@
 // You will have time to focus on it later.
 
 (function() {
+document.getElementById('run').addEventListener('click', function(){
 
-    // your code here
+
+
+  function random (min, max)
+{
+    let rand = Math.random() * (min-max);
+    numbers = rand -(rand*2);
+    return Math.floor (numbers);
+ } 
+   const tabNumber = [
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+       random(1, 100),
+  ];
+   document.getElementById('n-1').innerHTML= tabNumber[0];
+   document.getElementById('n-2').innerHTML= tabNumber[1];
+   document.getElementById('n-3').innerHTML = tabNumber[2];
+   document.getElementById('n-4').innerHTML = tabNumber[3];
+   document.getElementById('n-5').innerHTML = tabNumber[4];
+   document.getElementById('n-6').innerHTML = tabNumber[5];
+   document.getElementById('n-7').innerHTML = tabNumber[6];
+   document.getElementById('n-8').innerHTML = tabNumber[7];
+   document.getElementById('n-9').innerHTML = tabNumber[8];
+   document.getElementById('n-10').innerHTML = tabNumber[9];
+
+   const min = tabNumber.reduce((a, b) => Math.min(a, b))
+   document.getElementById('min').innerHTML = min;
+
+   const max = tabNumber.reduce((a, b) => Math.max(a,b)) //(function(a, b) {
+    document.getElementById('max').innerHTML = max;
+
+    const sum = tabNumber. reduce(function(a, b){
+        return a + b;
+    }, 0)
+    document.getElementById('sum').innerHTML = sum;
+   
+   
+    document.getElementById('average').innerHTML = sum/10;
+}) 
 
 })();
