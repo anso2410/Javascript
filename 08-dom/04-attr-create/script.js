ea97f61ef258cbd3e4225706d22c633e63a82a38
as <simana.annesophie@gmail.com>
2020-06-12 16:56:42 +0200

    08-3

--- 08-dom/04-attr-create/script.js
+++ 08-dom/04-attr-create/script.js
@@ -12,5 +12,29 @@
 (function() {
 
     // your code here
-
+    let value = document.getElementById('source');
+    let img = value.getAttribute("data-image");
+    let monImage = document.createElement('img');
+    
+   
+monImage.setAttribute('src', value);
+document.getElementById('target').append(img);
+// document.getElementById('source').remove();
 })();
+
+
+
+// (function() {
+
+//     let x = document.getElementById("source");
+//     let y = x.getAttribute("data-image");
+
+//     document.getElementById("source").remove();
+//     document.getElementById("target").innerHTML = "<img src='"+y+"'>";
+
+// })();    
+// let linkatt = document.getElementById("source").getAttribute("data-image");
+// let imgelem = document.createElement("img");
+// imgelem.setAttribute('src', linkatt);
+// document.getElementById("target").append(imgelem);
+// document.getElementsByClassName('material').item(0).removeChild(document.getElementById("source"));
